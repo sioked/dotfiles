c29da21c3dcdbcc309c4dbe5d034988f2f932209
Ed Siok <sioked@gmail.com>
2019-11-26 09:37:43 -0600

    In progress changes from remote ubuntu server

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,19 @@
-source $HOME/.aliases
-source $HOME/.config/github/keys.config
+if status is-interactive
+  source $HOME/.aliases
+  source $HOME/.config/github/keys.config
 
-set -gx PATH "/usr/local/sbin" $PATH
-export NVM_DIR="$HOME/.nvm"
-function nvm
-  bass source ~/.nvm/nvm.sh ';' nvm $argv
+  set -gx PATH "/usr/local/sbin" $PATH
+  export NVM_DIR="$HOME/.nvm"
+  function nvm
+    bass source ~/.nvm/nvm.sh ';' nvm $argv
+  end
+  # Set path for system wide tools to have access
+  nvm use default
+  export GPG_KEY=(tty)
+  source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+  fish_vi_key_bindings
+  set -gx PATH /usr/local/sbin /usr/local/opt/ruby/bin $HOME/Library/Python/3.7/bin $PATH
+
+  export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 end
-# Set path for system wide tools to have access
-nvm use default
-export GPG_KEY=(tty)
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-
-fish_vi_key_bindings
-set -gx PATH /usr/local/sbin /usr/local/opt/ruby/bin $HOME/Library/Python/3.7/bin $PATH
-
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
